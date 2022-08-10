79b6f594dbe7e973724b7f5357de7e229e9a210b
Shaevitzaiden <Aidenashaevitz@gmail.com>
2022-08-10 16:05:11 -0700

    Update processData-np.py
    better radii of sensing finding with some addition of manual trimming to remove outliers that don't get rejected automatically

--- processData-np.py
+++ processData-np.py
@@ -22,17 +22,18 @@ def remove_outliers(data):
     mean = np.mean(data)
     standard_deviation = np.std(data)
     distance_from_mean = abs(data - mean)
-    max_deviations = 3
+    max_deviations = 1
     not_outlier = distance_from_mean < max_deviations * standard_deviation
     no_outliers = data[not_outlier]
     return no_outliers
 
-def preprocess(bad_data):
+def preprocess(data):
+    bad_data = data.copy()
     bad_data[:,:,2] = bad_data[:,:,2].copy() - bad_data[:,:,3].copy()
     data = np.zeros((bad_data.shape[0],bad_data.shape[2]))
     for i, set in enumerate(bad_data[:]):
         data[i,:] = process_set(set)
-    data = np.flip(data,axis=0)
+    # data = np.flip(data,axis=0)
     return data
 
 def make_mesh(*data_sets, colors=("white","red","yellow"), edgecolors=('grey',"black"), line=None, fig=None, ax=None):
@@ -58,7 +59,7 @@ def generate_circle_array(radius, offset, height):
     return x, y, z
 
 
-def make_heatmaps(data1, data2, data3):
+def make_heatmaps(data1, data2, data3, c1):
     Z1 = data1[:,2]
     Z2 = data2[:,2]
     Z3 = data3[:,2]
@@ -66,18 +67,17 @@ def make_heatmaps(data1, data2, data3):
     min_val = np.min(np.hstack([Z1, Z2, Z3]))
     max_val = np.max(np.hstack([Z1, Z2, Z3]))
 
-    print(min_val)
-    print(max_val) 
-
     grid_dim = int(np.sqrt(data1.shape[0]))
     Z1 = Z1.reshape((grid_dim, grid_dim))
     Z2 = Z2.reshape((grid_dim, grid_dim))
     Z3 = Z3.reshape((grid_dim, grid_dim))
-
+    dims = Z1.shape
     f,(ax1,ax2,ax3, cax) = plt.subplots(1,4,gridspec_kw={'width_ratios': [4,4,4,1], "height_ratios": [1]}) #, gridspec_kw={'width_ratios':[1,1,1,0.08]})
     # ax1.get_shared_y_axes().join(ax2,ax3)
 
     g1 = sns.heatmap(Z1,cmap="YlGnBu",cbar=False,ax=ax1,square=True, vmin=min_val, vmax=max_val)
+    ax1.plot(c1[0]+dims[0]/2, c1[1]+dims[1]/2)
+    ax1.plot(dims[0]/2, dims[1]/2,'*k')
     g1.set_ylabel('')
     g1.set_xlabel('')
     g2 = sns.heatmap(Z2,cmap="YlGnBu",cbar=False,ax=ax2,square=True, vmin=min_val, vmax=max_val)
@@ -113,42 +113,41 @@ def plot_line_series(data):
 def diagonal_slice(data, plot=False):
     y_start = -100
     x_start = -100
-    n, r, c = data.shape
     data_slice = []
-    for i in range(data.shape[0]):
-        if (data[i,0,1] > y_start) and (data[i,0,0] > x_start):
-            data_slice.append(data[i])
-            y_start = data[i,0,1]
-            x_start = data[i,0,0]
+    for sample in data[:]:
+        if (sample[1] > y_start) and (sample[0] > x_start):
+            y_start = sample[1].copy()
+            x_start = sample[0].copy()
+            data_slice.append(sample)
     data_slice = np.array(data_slice)
     if plot:
-        plt.plot(np.sqrt(data_slice[:,:,0]**2+data_slice[:,:,1]**2), data_slice[:,:,2],'.')
+        plt.plot(np.sqrt(data_slice[:,0]**2+data_slice[:,1]**2), data_slice[:,2],'.')
         plt.hlines(0, 0, 10)
-        plt.vlines(0, 0, np.max(data_slice[:,:,2]))
+        plt.vlines(0, 0, np.max(data_slice[:,2]))
         plt.show()
     return data_slice
 
-
-def find_sensing_boundary(data_avg, threshold_percent):
-    data_avg = np.mean(data_slice, axis=1)
-    above_thresh_idx = data_avg[:,2] > threshold_percent*np.max(data_avg[:,2])
-    data_above_thresh = data_avg[above_thresh_idx]
+def find_sensing_boundary(data_avg, threshold_percent, manual_max_trim=None):
+    # data_avg = np.mean(data_slice, axis=1)
+    # manually trim out bugged data for the purpose of identifying sensing radius
+    data = data_avg.copy()
+    if manual_max_trim != None:
+        below_manul_max_idx = data[:,2] < manual_max_trim
+        data = data[below_manul_max_idx]
+    
+    above_thresh_idx = data[:,2] > threshold_percent*np.max(data[:,2])
+    data_above_thresh = data[above_thresh_idx]
     radius = (np.sqrt(data_above_thresh[0,0]**2+data_above_thresh[0,1]**2) + np.sqrt(data_above_thresh[-1,0]**2+data_above_thresh[-1,1]**2)) / 2
     height = np.min(data_above_thresh[:,2])
     center_idx = np.argmax(data_above_thresh[:,2])
-    center = data_above_thresh[center_idx,0:2]
-    return radius, height, center
+    return radius, height
 
 
 if __name__ == "__main__":
     data_10 = np.load("test_data_multi-sample/DS10_100g_50-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-10.npy")
     data_10_prep = preprocess(data_10)
-    data_slice = diagonal_slice(data_10, plot=True)
-    # print(data_slice)
-    # data_slice_prep = preprocess(data_slice)
-    # # print(data_slice_prep)
-    radius_of_sensing, height_of_radius, center = find_sensing_boundary(data_slice,0.05)
-    print(radius_of_sensing)
+   
+    # print(radius_of_sensing)
     # data_20 = np.load("test_data_multi-sample/DS20_100g_atm-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-20.npy")
     # data_20_prep = preprocess(data_20)
 
@@ -156,9 +155,15 @@ if __name__ == "__main__":
     # data_30_prep = preprocess(data_30)
    
 
-    # # ---------------------------
-    make_heatmaps(data_10_prep, data_10_prep, data_10_prep)
-    circle_points = generate_circle_array(radius_of_sensing, center, height_of_radius)
+    # # --------------------------- 
+    data_slice_10 = diagonal_slice(data_10_prep, plot=True)
+    radius_of_sensing_10, height_of_radius_10 = find_sensing_boundary(data_slice_10, 0.05, manual_max_trim=4)
+    print(radius_of_sensing_10)
+
+    center = (-0.05, -0.03)
+    circle_points = generate_circle_array(radius_of_sensing_10, center, height_of_radius_10)
+
+    make_heatmaps(data_10_prep, data_10_prep, data_10_prep, circle_points)
     make_mesh(data_10_prep,line=circle_points)
     # # # plt.xlabel("X")
     # X = data_10_prep[:,0]
