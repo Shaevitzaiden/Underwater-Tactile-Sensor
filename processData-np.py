786d4ef8ce37960af1d40cd7fb91873bf5a47c4f
Shaevitzaiden <Aidenashaevitz@gmail.com>
2022-10-05 17:16:05 -0700

    Update processData-np.py

--- processData-np.py
+++ processData-np.py
@@ -163,10 +163,16 @@ def find_sensing_boundary(data_avg, threshold_percent, manual_max_trim=None):
 if __name__ == "__main__":
     center = (-0.05, -0.03)
 
-    data_10 = np.load("test_data_multi-sample/DS10_atm_single_21x21_0.5mm_10-samples.npy")
+    # data_10 = np.load("test_data_multi-sample\DS20_100g_atm-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-20.npy")
+    # data_10_prep = preprocess(data_10)
+
+    # data_20 = np.load("test_data_multi-sample/DS20_100g_50-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-10.npy")
+    # data_20_prep = preprocess(data_20)
+
+    data_10 = np.load("test_data_multi-sample\DS10_100g_atm-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-5.npy")
     data_10_prep = preprocess(data_10)
-    # print(radius_of_sensing)
-    data_20 = np.load("test_data_multi-sample/DS20_100g_50-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-10.npy")
+
+    data_20 = np.load("test_data_multi-sample\DS10_100g_50-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-10.npy")
     data_20_prep = preprocess(data_20)
 
     # data_30 = np.load("test_data_multi-sample/DS10_100g_30-PSI_delta-0.5mm_thick-8mm_single-barometer-16_multi-sample-5.npy")
